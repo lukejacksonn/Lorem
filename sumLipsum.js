1fe219f3788c4c9c060dedd862a19ca1e7a9cbb3
lukejacksonn <lukejacksonn@gmail.com>
2013-05-05 16:32:59 +0200

    Update sumLipsum.js

--- sumLipsum.js
+++ sumLipsum.js
@@ -1,27 +1,37 @@
 jQuery.fn.sumLipsum = function (length) {
   
   var length = length-1 || Math.floor(Math.random()*50)+3;
-
   var lipsum = function() {
 
     var str = "Lorem";
-    var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt? ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     var l = lorem.split(" ");
+    var word;
     
     for(i = 0; i < length; i = i+1) {
     
-        var word = l[Math.floor(Math.random()*l.length)];
-        if(str.indexOf(".", str.length-1) !== -1) {
-          str +=  " " + word[0].toUpperCase()+word.slice(1);
-        }
-        else { str +=  " " + word.toLowerCase(); }
+      //Select random word from lorem
+      word = l[Math.floor(Math.random() * l.length)]; 
+      str += " ";
+      
+      //Append to output, capitalize first letter if necessary
+      str += (str.indexOf(".", str.length-2) !== -1 
+           || str.indexOf("?", str.length-2) !== -1) 
+            ? word[0].toUpperCase()+word.slice(1) 
+            : word.toLowerCase();
     }
-    str += ".";
+
+    //Append full stop to the end of string, strip punctuation if necessary
+    str = (str.indexOf(".", str.length-1) !== -1 
+        || str.indexOf(",", str.length-1) !== -1 
+        || str.indexOf("?", str.length-1) !== -1) 
+         ? str.slice(0,-1) + "." 
+         : str + ".";
+    
     return str;
   }
 
-  return this.each(function () {
-    $(this).text(lipsum());
-  });
-
+  //Populate self with lipsum snippit
+  return this.text(lipsum());
+  
 };
