d09587a28af27fcfb1d6482790f3c0c884ecae69
lukejacksonn <lukejacksonn@gmail.com>
2013-06-15 23:42:30 +0200

    Update lorem.js

--- lorem.js
+++ lorem.js
@@ -4,16 +4,16 @@
 
 		return this.each(function() {
 
-			var _length = $(this).attr('class').match(/lorem-\d+/)[0].split('-')[1] || _length || (Math.floor(Math.random()*50)+3);
+		var _length = $(this).attr('class').match(/lorem-\d+/)[0].split('-')[1] || _length || (Math.floor(Math.random()*50)+3);
 
-			var paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt? ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-			  , words = paragraph.split(" ")
-			  , word = ""
-			  , out = "Lorem"
+		var paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt? ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		  , words = paragraph.split(" ")
+		  , word = ""
+		  , out = "Lorem"
 
-			var charAtEndOfOut = function (char,step) {
-				return out.indexOf(char, out.length-step-1) !== -1
-			}
+		var charAtEndOfOut = function (char,step) {
+			return out.indexOf(char, out.length-step-1) !== -1
+		}
       		    
 		    for(var i = 0; i < _length; i = i+1) {
 		    
