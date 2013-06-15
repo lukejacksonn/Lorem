c986b8ab46c69fb7fbb9c90076cabbd78c47437d
lukejacksonn <lukejacksonn@gmail.com>
2013-06-16 00:01:11 +0200

    Update lorem.js

--- lorem.js
+++ lorem.js
@@ -3,7 +3,7 @@
     $.fn.lorem = function(_length) {
         return this.each(function() {
 
-            var _length = $(this).attr('class').match(/lorem-\d+/)[0].split('-')[1] || _length || (Math.floor(Math.random()*50)+3);
+            var _length = _length || $(this).attr('class').match(/lorem-\d+/)[0].split('-')[1] ||  (Math.floor(Math.random()*50)+3);
             var paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt? ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 	  , words = paragraph.split(" ")
 	  , word = ""
