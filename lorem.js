5e80125b8eb6996fcaa248e0f8492a92b1e4f265
Luke Jackson <lukejacksonn@gmail.com>
2015-04-30 20:01:06 +0100

    Changed from classes to data attribute selector

--- lorem.js
+++ lorem.js
@@ -1,51 +1,44 @@
-(function($){
+(function($) {
+  $.fn.lorem = function(_length) {
+    return this.each(function() {
 
-    $.fn.lorem = function(_length) {
+      // Amount of words required
+      var _length = _length || $(this).data('lorem') || (Math.floor(Math.random() * 50) + 3);
 
-        //Define Lorem Paragraph and split into words
-        var paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt? ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          , words = paragraph.split(" ")
+      // Helper functions
+      var charAtEndOfOut = function(char, step) {
+        return out.indexOf(char, out.length - step - 1) !== -1;
+      }
+      var randomWord = function() {
+        return words[Math.floor(Math.random() * (words.length - 1))];
+      }
+      var capitalize = function(word) {
+        return word[0].toUpperCase() + word.slice(1)
+      }
 
-        return this.each(function() {
+      // Dictionary of words
+      var paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt. ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        words = paragraph.split(" "),
+        word = "",
+        out = capitalize(randomWord());
 
-            //Establish word limit
-            var limit = _length 
-            || ($(this).attr('class') ? $(this).attr('class').match(/lorem-\d+/)[0].split('-')[1] : null) 
-            || (Math.floor(Math.random()*50)+3);
+      for (var i = 1; i < _length; i = i + 1) {
+        //Select random word from paragraph
+        word = randomWord();
+        out += " ";
 
-            //Setup out string, prepare for next word
-            var out = "Lorem", word = "";
+        //Append to out, capitalize first letter if necessary
+        out += (charAtEndOfOut('.', 1) || charAtEndOfOut('?', 1)) ? capitalize(word) : word.toLowerCase();
+      }
 
-            //Helper for finding character at position x from the end of out
-            var charAtEndOfOut = function (char,step) {
-            	return out.indexOf(char, out.length-step-1) !== -1
-            }
-      		
-            //Construct snippet of length limit  
-            for(var i = 1; i < limit; i = i+1) {
-		    
-                //Select random word from paragraph
-                word = words[Math.floor(Math.random() * words.length)]; 
-                out += " ";
+      //Append full stop to the end of string, strip punctuation if necessary
+      out = (charAtEndOfOut('.') || charAtEndOfOut(',') || charAtEndOfOut('?')) ? out.slice(0, -1) + "." : out + ".";
 
-                //Append to out, capitalize first letter if necessary
-                out += (charAtEndOfOut('.',1) ||  charAtEndOfOut('?',1)) 
-                ?  word[0].toUpperCase()+word.slice(1) 
-                :  word.toLowerCase();
-            }
+      $(this).text(out);
 
-            //Append full stop to the end of out string, strip punctuation if necessary
-            out = (charAtEndOfOut(',') || charAtEndOfOut('.')  || charAtEndOfOut('?')) 
-            ? out.slice(0,-1) + "." 
-            : out + ".";
+    });
+  }
 
-            //Set self text to out
-            $(this).text(out);
-		    
-        });
-    }
+  $("[data-lorem]").lorem();
 
-    //Target all 'lorem-' classes on load
-    $(function(){ $("*[class*='lorem-']").lorem(); });
-  
 })(jQuery);
